369a52b7c19411b54a932a4afadc544abb91fef4
vivianbarclay <vivian.m.barclay@gmail.com>
2025-01-23 17:05:33 +0000

    Second updates to umbrella

--- umbrella.rb
+++ umbrella.rb
@@ -5,17 +5,21 @@ require "dotenv/load"
 # Write your soltuion here!
 pp "Please input your location here:"
 
-user_loc = gets.chomp
+user_loc = "Chicago"
 
 gmaps_key = ENV.fetch("GMAPS_KEY")
 
-gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_loc}key=#{gmaps_key}"
+pp gmaps_key
+
+gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_loc}&key=#{gmaps_key}"
 
 raw_gmaps_url =  HTTP.get(gmaps_url)
 
-pp JSON.parse(raw_gmaps_url)
+parsed_gmaps_url = JSON.parse(raw_gmaps_url)
 
-#pp JSON.parse(gmaps_url)
+gmaps_results_array = parsed_gmaps_url.fetch("results")
+
+pp gmaps_results_array
 
 pirate_weather_key = ENV.fetch("PIRATE_WEATHER_KEY")
 
